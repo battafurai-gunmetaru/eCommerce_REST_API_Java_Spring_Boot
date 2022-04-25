de30ab7d9fc3171bc51f476a1b7eb92dbfa8f438
Brandyn Tse <brandyntse@protonmail.com>
2022-04-24 21:05:46 -0400

    creates CRUD endpoints for greetings

--- src/main/java/com/example/restgreeting/constants/StringConstants.java
+++ src/main/java/com/example/restgreeting/constants/StringConstants.java
@@ -6,5 +6,8 @@ public class StringConstants {
   public static final String QUERY_REQUEST = "Query request received for ";
   public static final String NOT_FOUND = "Could not locate resource: ";
   public static final String VALIDATION_ERROR = "Validation error";
+  public static final String UPDATE_REQUEST = "Put request received for ";
 
+  public static final String DELETE_REQUEST = "Delete request received for ";
+  public static final String POST_REQUEST = "Post request received for ";
 }
--- src/main/java/com/example/restgreeting/controllers/GreetingController.java
+++ src/main/java/com/example/restgreeting/controllers/GreetingController.java
@@ -1,18 +1,26 @@
 package com.example.restgreeting.controllers;
 
+import static com.example.restgreeting.constants.StringConstants.DELETE_REQUEST;
+import static com.example.restgreeting.constants.StringConstants.POST_REQUEST;
 import static com.example.restgreeting.constants.StringConstants.QUERY_REQUEST;
+import static com.example.restgreeting.constants.StringConstants.UPDATE_REQUEST;
 
 import com.example.restgreeting.models.Greeting;
 import com.example.restgreeting.services.GreetingService;
 import java.util.Date;
 import java.util.List;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,9 +41,31 @@ public class GreetingController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Greeting> getGreetingById(@PathVariable Long id) {
+  public ResponseEntity<Greeting> getGreetingById(@PathVariable Long id) throws Exception {
     logger.info(new Date() + QUERY_REQUEST + "greeting with id " + id);
 
     return new ResponseEntity<>(greetingService.getGreetingById(id), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Greeting> postGreeting(@Valid @RequestBody Greeting greeting) {
+    logger.info(new Date() + POST_REQUEST + "greeting");
+
+    return new ResponseEntity<>(greetingService.postGreeting(greeting), HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Greeting> updateGreetingById(@PathVariable Long id,
+      @Valid @RequestBody Greeting greeting) {
+    logger.info(new Date() + UPDATE_REQUEST + "greeting with id " + id);
+
+    return new ResponseEntity<>(greetingService.updateGreetingById(id, greeting), HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Greeting> deleteGreetingById(@PathVariable Long id) {
+    logger.info(new Date() + DELETE_REQUEST + "greeting with id " + id);
+
+    return new ResponseEntity<>(greetingService.deleteGreetingById(id), HttpStatus.NO_CONTENT);
   }
 }
